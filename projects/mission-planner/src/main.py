da8af8f4510c04ae3eafa2a85cbf3aa648cf4878
Anthony Lu <anthonylu7678@gmail.com>
2025-11-15 16:14:53 -0800

    Complete WebSocket fixes!

--- projects/mission-planner/src/main.py
+++ projects/mission-planner/src/main.py
@@ -1,164 +1,66 @@
 import sys
-#from multiprocessing import Process
-from threading import Thread
+import signal
+from threading import Thread, Event
 
 from server.utilities.connect_to_sysid import connect_to_sysid
-from server.utilities.request_message_streaming import set_message_streaming_rates
-
 from server.httpserver import HTTP_Server
 from server.status_wsclient import Status_Client
 
-# Process command line args
-production = True
-HOST, PORT, SOCKET_PORT = "localhost", 9000, 9001
-STATUS_HOST, STATUS_PORT = "localhost", 8000
-DISABLE_STATUS = False
-MAVLINK_CONNECTION_STRING = 'udpin:localhost:14551'
+stop_event = Event()
+
+def run_http_server(mav_connection, production, host, port):
+    http_server = HTTP_Server(mav_connection)
+    http_server.serve_forever(production, host, port)
+
+def run_status_client(mav_connection, production, host, port):
+    ws_client = Status_Client(mav_connection)
+    ws_client.connect_to(production, host, port)
+
+def signal_handler(sig, frame):
+    print("Caught Ctrl+C, shutting down...")
+    stop_event.set()
 
 if __name__ == "__main__":
-    # Extract arguments
-    arguments = {}
-    for arg in sys.argv:
-        k, *v = arg.split('=', 1)
-        arguments[k] = v
-
-    # Process options
-    if "--test" in arguments.keys():
-        print("starting test environment")
-        
-    if '--dev' in arguments.keys():
-        print("Starting server in development")
-        production = False
-    else:
-        print("Starting server in production")
-    
-    if '--port' in arguments.keys():
-        PORT = int(arguments['--port'][0])
-
-    if '--socket-port' in arguments.keys():
-        SOCKET_PORT = int(arguments['--socket-port'][0])
-
-    if '--status-host' in arguments.keys():
-        STATUS_HOST = arguments['--status-host'][0]
-    
-    if '--status-port' in arguments.keys():
-        STATUS_PORT = arguments['--status-port'][0]
-
-    if '--disable-status' in arguments.keys():
-        DISABLE_STATUS = True
-
-    if '--mavlink-conn' in arguments.keys():
-        MAVLINK_CONNECTION_STRING = arguments['--mavlink-conn'][0]
-        
-    print(f"Starting... HTTP server listening at {HOST}:{PORT}. " + ("" if DISABLE_STATUS else f"Status WS connecting to {STATUS_HOST}:{STATUS_PORT}."))
+    # Simplified argument parsing here, replace with your own
+    production = True
+    HOST, PORT = "localhost", 9000
+    STATUS_HOST, STATUS_PORT = "localhost", 8000
+    DISABLE_STATUS = False
+    MAVLINK_CONNECTION_STRING = 'udpin:localhost:14551'
 
     mav_connection = connect_to_sysid(MAVLINK_CONNECTION_STRING, 1)
-    if mav_connection == None:
-        print(f"MAV connection failed")
-    else:
-        print(f"MAV connection successful")
-    
-    # set_message_streaming_rates(mav_connection) # optional - set update rate (applies to both MissionPlanner and this server)
+    if mav_connection is None:
+        print("MAV connection failed")
+        sys.exit(1)
+    print("MAV connection successful")
 
-    # Create MPS server
-    gcmh = HTTP_Server(mav_connection)
-    gcmh.serve_forever(True, HOST, 9000)
-    # Create websocket status client
-    wsc = Status_Client(mav_connection)
-    wsc.connect_to(True, STATUS_HOST, STATUS_PORT)
+    # Setup Ctrl+C handler
+    signal.signal(signal.SIGINT, signal_handler)
 
-    # gcmh thread
-    gcmh_thread = Thread(target=gcmh.serve_forever, args=[production, HOST, PORT])
+    # Start HTTP server thread
+    http_thread = Thread(target=run_http_server, args=(mav_connection, production, HOST, PORT))
+    http_thread.start()
 
-    # #status websocket client thread
-    # if not DISABLE_STATUS:
-    #     skth_thread = Thread(target=skth.connect_to, args=[production, STATUS_HOST, STATUS_PORT])
+    # Start status websocket client thread if enabled
+    if not DISABLE_STATUS:
+        status_thread = Thread(target=run_status_client, args=(mav_connection, production, STATUS_HOST, STATUS_PORT))
+        status_thread.start()
 
-    print("\nStarting threads...\n")
+    try:
+        # Wait for stop event
+        while not stop_event.is_set():
+            stop_event.wait(1)
+    except KeyboardInterrupt:
+        pass
 
-    gcmh_thread.start()
-    # if not DISABLE_STATUS:
-    #     skth_thread.start()
+    print("Shutting down threads...")
 
-    gcmh_thread.join()
-    # if not DISABLE_STATUS:
-    #     skth_thread.join()
+    # Flask's server doesn't provide a clean stop API,
+    # but exiting main thread will kill the app.
+    # You might want to set socketio.stop() inside HTTP_Server to support this.
 
+    http_thread.join(timeout=3)
+    if not DISABLE_STATUS:
+        status_thread.join(timeout=3)
 
-# import sys
-# #from multiprocessing import Process
-# from threading import Thread
-
-# from server.common.sharedobject import SharedObject
-# from server.gcomhandler import G
-# from server.status_wsclient import Sc_Client
-# COM_Server
-# from server.mps_server import MPS_Server
-# # Process command line args
-# production = True
-# HOST, PORT, SOCKET_PORT = "localhost", 9000, 9001
-# STATUS_HOST, STATUS_PORT = "localhost", 1323
-# DISABLE_STATUS = False
-# if __name__ == "__main__":
-#     # Extract arguments
-#     arguments = {}
-#     for arg in sys.argv:
-#         k, *v = arg.split('=', 1)
-#         arguments[k] = v
-
-#     # Process options
-#     if "--test" in arguments.keys():
-#         print("starting test environment")
-        
-#     if '--dev' in arguments.keys():
-#         print("Starting server in development")
-#         production = False
-#     else:
-#         print("Starting server in production")
-    
-#     if '--port' in arguments.keys():
-#         PORT = int(arguments['--port'][0])
-
-#     if '--socket-port' in arguments.keys():
-#         SOCKET_PORT = int(arguments['--socket-port'][0])
-
-#     if '--status-host' in arguments.keys():
-#         STATUS_HOST = arguments['--status-host'][0]
-    
-#     if '--status-port' in arguments.keys():
-#         STATUS_PORT = arguments['--status-port'][0]
-
-#     if '--disable-status' in arguments.keys():
-#         DISABLE_STATUS = True
-        
-#     print(f"Starting... HTTP server listening at {HOST}:{PORT}. " + ("" if DISABLE_STATUS else f"Status WS connecting to {STATUS_HOST}:{STATUS_PORT}."))
-
-#     # Instantiate shared object
-#     so = SharedObject()
-
-#     # Create server
-#     mpss = MPS_Server(so)
-#     gcmh = GCOM_Server(so)
-#     skth = Status_Client(so)
-
-#     # mpss thread
-#     mpss_thread = Thread(target=mpss.serve_forever)
-
-#     # gcmh thread
-#     gcmh_thread = Thread(target=gcmh.serve_forever, args=[production, HOST, PORT])
-
-#     #status websocket client thread
-#     if not DISABLE_STATUS:
-#         skth_thread = Thread(target=skth.connect_to, args=[production, STATUS_HOST, STATUS_PORT])
-
-#     print("\nStarting threads...\n")
-
-#     mpss_thread.start()
-#     gcmh_thread.start()
-#     if not DISABLE_STATUS:
-#         skth_thread.start()
-
-#     mpss_thread.join()
-#     gcmh_thread.join()
-#     if not DISABLE_STATUS:
-#         skth_thread.join()
+    print("Exited cleanly.")
