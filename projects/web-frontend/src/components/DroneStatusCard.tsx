77efb3a79d1d8f4c7d4b311183a666ef1e179e72
Michael Dickinson <michael.dickinson3949@gmail.com>
2025-11-28 22:11:23 -0800

    more specific speed values

--- projects/web-frontend/src/components/DroneStatusCard.tsx
+++ projects/web-frontend/src/components/DroneStatusCard.tsx
@@ -17,8 +17,8 @@ const roundValues = (
         latitude: Math.round(data.latitude * 1000000) / 1000000,
         longitude: Math.round(data.longitude * 1000000) / 1000000,
         altitude: Math.round(data.altitude),
-        verticalSpeed: Math.round(data.vertical_velocity),
-        speed: Math.round(data.velocity),
+        verticalSpeed: Math.round(data.vertical_velocity * 100) / 100,
+        speed: Math.round(data.velocity * 100) / 100,
         heading: Math.round(data.heading),
         voltage: Math.round(data.voltage * 100) / 100,
     } satisfies AircraftStatus;
