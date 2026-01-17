bb877c25b5c16c934daf89af303964cec6bd0717
Chaitanya Choudhary <chaitanya.rook@gail.com>
2026-01-17 15:22:13 -0800

    pub sub working

--- projects/web-backend/src/mqtt/models.py
+++ projects/web-backend/src/mqtt/models.py
@@ -4,19 +4,31 @@ from paho import mqtt
 
 
 class MQTT(models.Model):
-    def __init__(self, client_id="", userdata=None):
-        self.client = paho.Client(
-            client_id=client_id, userdata=userdata, protocol=paho.MQTTv5
-        )
+    def on_connect(client, userdata, flags, rc, properties=None):
+        print("CONNACK received with code %s." % rc)
 
-    def connect(self):
-        self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
-        self.client.username_pw_set("Apple", "Dog")
-        self.client.connect("159c683892ec4dbbaefa283364f7b6b3.s1.eu.hivemq.cloud", 8883)
+    def on_publish(client, userdata, mid, properties=None):
+        print("mid: " + str(mid))
 
-    def publish(self, topic, payload_text, QOS_Level):
-        self.client.publish(topic, payload=payload_text, qos=QOS_Level)
+    def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+        print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-tempqtt = MQTT()
-tempqtt.publish("topic/temperatures", "bonky", 1)
+    client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+    client.on_connect = on_connect
+
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    client.username_pw_set("Apple", "Doggy123")
+    client.connect("159c683892ec4dbbaefa283364f7b6b3.s1.eu.hivemq.cloud", 8883)
+
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.on_publish = on_publish
+
+    client.subscribe("encyclopedia/#", qos=1)
+
+    client.publish("encyclopedia/temperature", payload="hot", qos=1)
+
+    client.loop_forever()
