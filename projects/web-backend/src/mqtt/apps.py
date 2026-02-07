b490cc625192697be14063ba7f36ec55da99a193
Chaitanya Choudhary <chaitanya.rook@gail.com>
2026-02-07 02:11:34 -0800

    mqtt publisher file

--- projects/web-backend/src/mqtt/apps.py
+++ projects/web-backend/src/mqtt/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class MqttConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mqtt'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mqtt"
+
+    def ready(self):
+        from . import publisher
---
+++ projects/web-backend/src/mqtt/publisher.py
@@ -0,0 +1,46 @@
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+MQTT_BROKER = os.getenv("MQTT_BROKER")
+MQTT_PORT = int(os.getenv("MQTT_PORT"))
+MQTT_USERNAME = os.getenv("MQTT_USERNAME")
+MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
+MQTT_TOPIC_CMD = os.getenv("MQTT_TOPIC")
+MQTT_TOPIC_STATUS = os.getenv("MQTT_TOPIC_STATUS")
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker")
+        client.subscribe(MQTT_TOPIC_CMD)
+        client.subscribe(MQTT_TOPIC_STATUS)
+    else:
+        print(f"Connection failed with code {rc}")
+
+
+def on_message(client, userdata, msg):
+    try:
+        message = json.loads(msg.payload.decode("utf-8"))
+        print(f"Received message: {message}")
+
+    except json.JSONDecodeError:
+        print("Error decoding MQTT message")
+
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected from MQTT Broker")
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT)
+
+client.loop_start()
