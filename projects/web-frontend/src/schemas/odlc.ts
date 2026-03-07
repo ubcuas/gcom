ef9532facbdfd7ebebfc5fdc6f7e148d6f60eba5
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-03-07 15:41:36 -0800

    add depth_data to odlc images type

--- projects/web-frontend/src/schemas/odlc.ts
+++ projects/web-frontend/src/schemas/odlc.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 export const OdlcImageSchema = z.object({
-    image_data: z.string(), // Base64-encoded image data
+    image_data: z.string(), // Base64-encoded JPEG color image
+    depth_data: z.string().nullable(), // Base64-encoded 16UC1 PNG depthmap, or null if unavailable
+    // So the PNG isn't an image but instead each pixel is a 16-bit unsigned integer representing depth in millimeters. We can decode this to get depth values.
     color_detection: z.tuple([z.number().int(), z.number().int(), z.number().int()]),
     bounding_box: z.array(z.tuple([z.number(), z.number()])),
     confidence_level: z.number().int(),
