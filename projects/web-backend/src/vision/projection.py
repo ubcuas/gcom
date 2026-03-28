33e26684fe8de62bccc5e19160732b6b0f6ca50f
Michael Dickinson <michael.dickinson3949@gmail.com>
2026-03-28 11:36:55 -0700

    format

--- projects/web-backend/src/vision/projection.py
+++ projects/web-backend/src/vision/projection.py
@@ -1,13 +1,11 @@
-import math
-
 FLT_EPSILON = 1e-6
 
 # Fixed camera intrinsics (Brown-Conrady / plumb_bob model)
 CAMERA_INTRINSICS = {
-    'fx': 643.4216918945312,
-    'fy': 642.5718994140625,
-    'ppx': 638.689453125,
-    'ppy': 383.8777160644531,
+    "fx": 643.4216918945312,
+    "fy": 642.5718994140625,
+    "ppx": 638.689453125,
+    "ppy": 383.8777160644531,
 }
 
 # Brown-Conrady distortion coefficients [k1, k2, p1, p2, k3]
@@ -29,8 +27,8 @@ def deproject_pixel_to_point(pixel: list[float], depth: float) -> list[float]:
 
     Re-implemented from https://github.com/realsenseai/librealsense/blob/78cb605b11f5ba80176e7b8d70292f76ba625565/src/rs.cpp#L4273
     """
-    x = (pixel[0] - CAMERA_INTRINSICS['ppx']) / CAMERA_INTRINSICS['fx']
-    y = (pixel[1] - CAMERA_INTRINSICS['ppy']) / CAMERA_INTRINSICS['fy']
+    x = (pixel[0] - CAMERA_INTRINSICS["ppx"]) / CAMERA_INTRINSICS["fx"]
+    y = (pixel[1] - CAMERA_INTRINSICS["ppy"]) / CAMERA_INTRINSICS["fy"]
 
     xo = x
     yo = y
@@ -38,9 +36,20 @@ def deproject_pixel_to_point(pixel: list[float], depth: float) -> list[float]:
     if not _is_distortion_zero(DISTORTION_COEFFS):
         for _ in range(10):
             r2 = x * x + y * y
-            icdist = 1.0 / (1 + ((DISTORTION_COEFFS[4] * r2 + DISTORTION_COEFFS[1]) * r2 + DISTORTION_COEFFS[0]) * r2)
-            delta_x = 2 * DISTORTION_COEFFS[2] * x * y + DISTORTION_COEFFS[3] * (r2 + 2 * x * x)
-            delta_y = 2 * DISTORTION_COEFFS[3] * x * y + DISTORTION_COEFFS[2] * (r2 + 2 * y * y)
+            icdist = 1.0 / (
+                1
+                + (
+                    (DISTORTION_COEFFS[4] * r2 + DISTORTION_COEFFS[1]) * r2
+                    + DISTORTION_COEFFS[0]
+                )
+                * r2
+            )
+            delta_x = 2 * DISTORTION_COEFFS[2] * x * y + DISTORTION_COEFFS[3] * (
+                r2 + 2 * x * x
+            )
+            delta_y = 2 * DISTORTION_COEFFS[3] * x * y + DISTORTION_COEFFS[2] * (
+                r2 + 2 * y * y
+            )
             x = (xo - delta_x) * icdist
             y = (yo - delta_y) * icdist
 
